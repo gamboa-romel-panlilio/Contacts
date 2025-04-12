d1fefcc77f7a226501c1d11382a1b1fc76e23c02
gamboa-romel-panlilio <melgrowtopia029@gmail.com>
2025-04-12 14:33:11 +0800

    commit by romel

--- lib/contact.dart
+++ lib/contact.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
+import 'package:url_launcher/url_launcher.dart';
 import 'variables.dart';
+import 'package:flutter/material.dart';
 
 class Contact extends StatefulWidget {
   const Contact({super.key});
@@ -12,13 +14,37 @@ class _ContactState extends State<Contact> {
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
-        navigationBar: CupertinoNavigationBar(
 
-        ),
         child: SafeArea(child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Text('$name')
-      ],
-    )));
-  }
-}
+
+        Stack(
+          alignment: Alignment.bottomCenter,
+          children: [
+
+            Image.network(photo, width: double.infinity, fit: BoxFit.fill, height: 300,),
+            Positioned(
+              top: -2,
+                left: -15,
+                child: Row(
+              children: [
+                CupertinoButton(child: Icon(CupertinoIcons.chevron_back, color: CupertinoColors.white,), onPressed: (){
+                  Navigator.pop(context);
+                })
+              ],
+            )),
+            Positioned(
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+
+                      Text("last used: ", style: TextStyle(fontSize: 12, fontWeight: FontWeight.w200),),
+                      Container(
+                          padding: EdgeInsets.fromLTRB(5, 0, 5, 0),
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(5),
+                              color: CupertinoColors.white
+                          ),
