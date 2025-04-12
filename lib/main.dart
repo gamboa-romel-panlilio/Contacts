99726a2e3d0560ac04e69ac62d0f09b6ce7d7e14
gamboa-romel-panlilio <melgrowtopia029@gmail.com>
2025-04-12 14:29:11 +0800

    commit by romel

--- lib/main.dart
+++ lib/main.dart
@@ -38,3 +38,80 @@ class _HomepageState extends State<Homepage> {
        });
      }
     super.initState();
+ }
+  TextEditingController _fname = TextEditingController();
+  TextEditingController _lname = TextEditingController();
+  TextEditingController _phone = TextEditingController();
+  TextEditingController _email = TextEditingController();
+  TextEditingController _url = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+         navigationBar: CupertinoNavigationBar(
+           trailing: CupertinoButton(child: Icon(CupertinoIcons.add), onPressed: (){
+           showCupertinoModalPopup(context: context, builder: (context){
+             return CupertinoActionSheet(
+               title: Row(
+                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                 children: [
+                   CupertinoButton(child: Text('Cancle'), onPressed: (){
+                     Navigator.pop(context);
+                   }),
+                   Text('New Contact'),
+                   CupertinoButton(child: Text('Done'), onPressed: (){
+                     setState(() {
+                       contacts.add(  {
+                         "name" : _fname.text + " " + _lname.text,
+                         "phone" : _phone.text,
+                         "email" : _email.text,
+                         "url" : _url.text,
+                         "photo" : "https://th.bing.com/th/id/OIP.v9xx5HA2kWMXMDxIms_86wHaLI?rs=1&pid=ImgDetMain"
+                       },);
+                       box.put('contacts', contacts);
+                       print(box.get('contacts'));
+
+                     });
+                     _fname.text = "";
+                     _lname.text = "";
+                     _phone.text = "";
+                     _email.text = "";
+                     _url.text = "";
+
+                     Navigator.pop(context);
+                   }),
+                 ],
+               ),
+
+               message: Column(
+                 children: [
+                   Icon(CupertinoIcons.person_circle_fill, color: CupertinoColors.systemGrey, size: 200,),
+                   CupertinoButton(child: Text('Add Photo'), onPressed: (){
+
+                   }),
+                   Container(
+                     decoration: BoxDecoration(
+                       color: CupertinoColors.systemGrey.withOpacity(0.1),
+                     ),
+                     child: Column(
+                       children: [
+                         CupertinoTextField(
+                           controller: _fname,
+                           placeholder: 'First Name',
+                           decoration: BoxDecoration(
+                             color: CupertinoColors.systemGrey.withOpacity(0.0),
+                           ),
+                         ),
+                         Divider(color: CupertinoColors.systemGrey.withOpacity(0.2),),
+                         CupertinoTextField(
+                           controller: _lname,
+                           placeholder: 'Last Name',
+                           decoration: BoxDecoration(
+                             color: CupertinoColors.systemGrey.withOpacity(0.0),
+                           ),
+                         ),
+
+
+                       ],
+                     ),
+                   ),
