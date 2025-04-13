cf8c76813f059b237ad1cb7825b8429b866d4fe6
gamboa-romel-panlilio <melgrowtopia029@gmail.com>
2025-04-13 12:56:50 +0800

    add pictures and fix the code by romel

--- lib/main.dart
+++ lib/main.dart
@@ -151,3 +151,144 @@ class _HomepageState extends State<Homepage> {
                                     CupertinoIcons.person_circle_fill,
                                     color: CupertinoColors.systemGrey,
                                     size: 200,
+      ),
+                                  CupertinoButton(child: Text('Add Photo'), onPressed: () {}),
+                                  Container(
+                                    decoration: BoxDecoration(
+                                      color: CupertinoColors.systemGrey.withOpacity(0.1),
+                                    ),
+                                    child: Column(
+                                      children: [
+                                        CupertinoTextField(
+                                          controller: _fname,
+                                          placeholder: 'First Name',
+                                          decoration: BoxDecoration(
+                                            color: CupertinoColors.systemGrey.withOpacity(0.0),
+                                          ),
+                                        ),
+                                        Divider(color: CupertinoColors.systemGrey.withOpacity(0.2)),
+                                        CupertinoTextField(
+                                          controller: _lname,
+                                          placeholder: 'Last Name',
+                                          decoration: BoxDecoration(
+                                            color: CupertinoColors.systemGrey.withOpacity(0.0),
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                  SizedBox(height: 20),
+                                  Column(
+                                    children: _phoneControllers.map((controller) {
+                                      return Padding(
+                                        padding: const EdgeInsets.only(bottom: 10.0),
+                                        child: CupertinoTextField(
+                                          prefix: Icon(
+                                            CupertinoIcons.phone_fill,
+                                            color: CupertinoColors.systemGreen,
+                                          ),
+                                          controller: controller,
+                                          placeholder: 'Phone Number',
+                                          keyboardType: TextInputType.phone,
+                                          decoration: BoxDecoration(
+                                            color: CupertinoColors.systemGrey.withOpacity(0.1),
+                                          ),
+                                        ),
+                                      );
+                                    }).toList(),
+                                  ),
+                                  CupertinoButton(
+                                    padding: EdgeInsets.zero,
+                                    child: Container(
+                                      padding: EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+                                      decoration: BoxDecoration(
+                                        color: CupertinoColors.systemGrey.withOpacity(0.1),
+                                        borderRadius: BorderRadius.circular(10),
+                                      ),
+                                      child: Row(
+                                        mainAxisAlignment: MainAxisAlignment.start,
+                                        children: [
+                                          Icon(CupertinoIcons.add_circled_solid, color: CupertinoColors.systemGreen),
+                                          SizedBox(width: 5),
+                                          Text('Add Phone', style: TextStyle(fontSize: 16)),
+                                        ],
+                                      ),
+                                    ),
+                                    onPressed: _addPhoneFieldLocal,
+                                  ),
+                                  SizedBox(height: 20),
+                                  Column(
+                                    children: _emailControllers.map((controller) {
+                                      return Padding(
+                                        padding: const EdgeInsets.only(bottom: 10.0),
+                                        child: CupertinoTextField(
+                                          prefix: Icon(
+                                            CupertinoIcons.mail_solid,
+                                            color: CupertinoColors.systemGreen,
+                                          ),
+                                          controller: controller,
+                                          placeholder: 'Email Address',
+                                          keyboardType: TextInputType.emailAddress,
+                                          decoration: BoxDecoration(
+                                            color: CupertinoColors.systemGrey.withOpacity(0.1),
+                                          ),
+                                        ),
+                                      );
+                                    }).toList(),
+                                  ),
+                                  CupertinoButton(
+                                    padding: EdgeInsets.zero,
+                                    child: Container(
+                                      padding: EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+                                      decoration: BoxDecoration(
+                                        color: CupertinoColors.systemGrey.withOpacity(0.1),
+                                        borderRadius: BorderRadius.circular(10),
+                                      ),
+                                      child: Row(
+                                        mainAxisAlignment: MainAxisAlignment.start,
+                                        children: [
+                                          Icon(CupertinoIcons.add_circled_solid, color: CupertinoColors.systemGreen),
+                                          SizedBox(width: 5),
+                                          Text('Add Email', style: TextStyle(fontSize: 16)),
+                                        ],
+                                      ),
+                                    ),
+                                    onPressed: _addEmailFieldLocal,
+                                  ),
+                                  SizedBox(height: 20),
+                                  Column(
+                                    children: _urlControllers.map((controller) {
+                                      return Padding(
+                                        padding: const EdgeInsets.only(bottom: 10.0),
+                                        child: CupertinoTextField(
+                                          prefix: Icon(
+                                            CupertinoIcons.link,
+                                            color: CupertinoColors.systemGreen,
+                                          ),
+                                          controller: controller,
+                                          placeholder: 'URL',
+                                          keyboardType: TextInputType.url,
+                                          decoration: BoxDecoration(
+                                            color: CupertinoColors.systemGrey.withOpacity(0.1),
+                                          ),
+                                        ),
+                                      );
+                                    }).toList(),
+                                  ),
+                                  CupertinoButton(
+                                    padding: EdgeInsets.zero,
+                                    child: Container(
+                                      padding: EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+                                      decoration: BoxDecoration(
+                                        color: CupertinoColors.systemGrey.withOpacity(0.1),
+                                        borderRadius: BorderRadius.circular(10),
+                                      ),
+                                      child: Row(
+                                        mainAxisAlignment: MainAxisAlignment.start,
+                                        children: [
+                                          Icon(CupertinoIcons.add_circled_solid, color: CupertinoColors.systemGreen),
+                                          SizedBox(width: 5),
+                                          Text('Add URL', style: TextStyle(fontSize: 16)),
+                                        ],
+                                      ),
+                                    ),
